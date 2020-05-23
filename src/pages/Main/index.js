a52fc74b40bbe7b983e60dc4f8c0aeff7fc3af4d
Marcos Lucas <marcosvaldeni@gmail.com>
2020-05-23 19:37:38 +0100

    Styled Components

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+import { Title } from './style';
+
 export default function Main() {
-  return <h1>Main</h1>;
+  return (
+    <Title error={false}>
+      Main
+      <small>test</small>
+    </Title>
+  );
 }
